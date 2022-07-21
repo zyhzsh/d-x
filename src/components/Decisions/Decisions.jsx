60654d2fa4a6f57cd9143572e85ce3b0097babbc
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-07-21 20:49:45 +0200

    update introduction

--- src/components/Decisions/Decisions.jsx
+++ src/components/Decisions/Decisions.jsx
@@ -21,32 +21,42 @@ const Decisions = () => {
 const Introduction = ()=>{
   return <Grid item xs={11}>
       <Typography variant='h4' sx={{textAlign:'center'}}>
-        What is decision-martix?
+        What is decision-martix ?
       </Typography>
       <Typography variant='h6'>
       <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      A decision matrix is a list of values in rows and columns that allows an analyst to systematically identify, analyze, and rate the performance of the relationship between sets of values and information.
+      </Typography>
+      <Typography variant='h4' sx={{textAlign:'center'}}>
+        What is weighted decision-martix ?
+      </Typography>
+      <Typography variant='h6'>
+      <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
+      A weighted decision matrix should be used to reach an answer when you have a choice between a number of different options that provide a solution to your question or problem. Furthermore, you need to consider each option against a number of factors involved in the decision-making process. For example: when choosing a holiday, you have many potential destinations to choose between and must consider each against factors such as cost and the weather.
       </Typography>
       <Typography variant='h4' sx={{textAlign:'center'}}>
        When to use it ? 
       </Typography>
       <Typography variant='h6'>
       <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </Typography>
-      <Typography variant='h4' sx={{textAlign:'center'}}>
-       How to use it ? 
-      </Typography>
-      <Typography variant='h6'>
+      Some decisions are easy, and some are much more difficult because of the number of options to choose between and the factors involved in the decision. Do I eat an apple or an orange is a simple question to answer and we're guessing not many people would need a weighted decision matrix for that one, but what if you're deciding something much more complex? For example, do you need to pick a school for your child to attend or do you need to decide which software solution meets your employer's complex needs?      
+      <br/>
       <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      Use cases: <br/>
+      <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
+      - Multiple-criteria decision analysis <br/>
+      <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
+      - Belief decision matrix  <br/>
+   
       </Typography>
       <Typography variant='h4' sx={{textAlign:'center'}}>
        Reference:
       </Typography>
-      <Typography variant='h6'>
+      <Typography variant='body1'>
       <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      Wikipedia, [Decision matrix](5th July 2022) Retrieved from:<a href='https://en.wikipedia.org/wiki/Decision_matrix'>https://en.wikipedia.org/wiki/Decision_matrix</a> <br/>  <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
+      Wikipedia, [Evidential reasoning approach] (5th July 2022) Retrieved from:<a href='https://en.wikipedia.org/wiki/Evidential_reasoning_approach'>https://en.wikipedia.org/wiki/Evidential_reasoning_approach</a><br/>  <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
+      Wikipedia, [When to use a Weighted Decision Matrix] (5th July 2022) Retrieved from:<a href='https://www.weighteddecision.com/when-to-use-a-weighted-decision-matrix/'>https://www.weighteddecision.com/when-to-use-a-weighted-decision-matrix/</a> <br/>  <span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span><span>&nbsp;&nbsp;</span>
       </Typography>
     </Grid>
 }
