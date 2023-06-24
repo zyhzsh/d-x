d38fd79216d339f02744d0e853aa4a16203c94eb
Shenghang Zhu <443880@student.fontys.nl>
2023-06-24 15:55:53 +0200

    change input from number to Slider

--- src/components/DecisionBoard/DecisionBoard.jsx
+++ src/components/DecisionBoard/DecisionBoard.jsx
@@ -1,76 +1,91 @@
-import React,{useContext} from 'react'
-import { Grid ,Typography} from '@mui/material'
+import React, { useContext } from 'react'
+import { Grid, Typography, Slider } from '@mui/material'
 import { DecisionsContext } from '../../context/DecisionsContext'
 import { DataGrid } from '@mui/x-data-grid';
 
 
-const DecisionBoard = ({decisionId,options}) => {
-  const {UpdateFactorScore,FindDecision} = useContext(DecisionsContext);
+const DecisionBoard = ({ decisionId, options }) => {
+  const { UpdateFactorScore, FindDecision } = useContext(DecisionsContext);
 
 
   const column = [
-    { 
-      filed:'name',
-      headerName: <Typography variant='h6'>Option</Typography>, 
-      headerAlign:'center',
-      align:'center',
+    {
+      filed: 'name',
+      headerName: <Typography variant='h6'>Option</Typography>,
+      headerAlign: 'center',
+      align: 'center',
       width: 300,
-      flex:1,
-      renderCell:(params)=>{
+      flex: 1,
+      renderCell: (params) => {
         return <Typography variant='body1'><strong>{params.row.name}</strong></Typography>
       }
     },
-    ...FindDecision(decisionId).factors.map((f=>{
+    ...FindDecision(decisionId).factors.map((f => {
       return {
-        field:f.name,
+        field: f.name,
         headerName: <Typography variant='h6'>{f.name}</Typography>,
-        headerAlign:'center',
-        align:'center',
+        headerAlign: 'center',
+        align: 'center',
         width: 200,
-        flex:1,
+        flex: 1,
         editable: true,
         type: "number",
-        renderCell:(params)=>{
-        let factor = params.row.factors.find(fa=>fa.id===f.id);
-        return <Typography variant='body1'><strong>{factor.score}</strong></Typography>;
-      }
+        renderCell: (params) => {
+          let factor = params.row.factors.find(fa => fa.id === f.id);
+          let optionId = params.row.id;
+          return <Slider
+            key={`slider-${'sdd'}`}
+            size="small"
+            sx={{ ml: 1, width: '70%' }}
+            // TODO: Fix Error :MUI: A component is changing the default value state of an uncontrolled Slider after being initialized. To suppress this warning opt to use a controlled Slider.
+            defaultValue={factor.weight}
+            valueLabelDisplay="auto"
+            // onChange={(e)=>processChange(factor.id,e.target.value)}
+            onChangeCommitted={(_, value) => {
+              UpdateFactorScore(decisionId, optionId, factor.id, value);
+            }
+            }
+          />
+          //return <Typography variant='body1'><strong>{factor.score}</strong></Typography>;
+        }
       }
     }))
   ];
 
-  
+
   return (
-    <Grid 
-      container 
+    <Grid
+      container
       flexDirection='column'
-      item 
-      sx={{  
-      height: '360px',
-      overflow: 'auto',
-      gap: 0.5
+      item
+      sx={{
+        height: '700px',
+        overflow: 'auto',
+        gap: 0.5
       }} >
       <Grid item container justifyContent='center' alignItems='center'>
-       <Grid item  container justifyContent='center' alignItems='center' xs={12} ><Typography variant="h6" >Scores</Typography></Grid>
+        <Grid item container justifyContent='center' alignItems='center' xs={12} ><Typography variant="h6" >Scores</Typography></Grid>
       </Grid>
-    {options.length !==0?
-      <DataGrid
-        columns={column}
-        rows={options}
-        hideFooter={true}
-        onCellEditCommit={(values,_)=>{
-           let value = values.value;
-           if(typeof value !== 'number') return;
-           if(value > 100) value = 100;
-           if(value < 0) value = 0;
-           let optionId = values.row.id;
-           let factorName = values.field;
-           let factorId = values.row.factors.find((f=>f.name===factorName)).id;
-           UpdateFactorScore(decisionId,optionId,factorId,value);
-        }}
-      />:
-    <Grid item  container justifyContent='center' alignItems='center' ><Typography variant="body1" >No content</Typography></Grid>
-    }
-  </Grid>
+      {options.length !== 0 ?
+        <DataGrid
+          rowHeight={100}
+          columns={column}
+          rows={options}
+          hideFooter={true}
+        // onCellEditCommit={(values, _) => {
+        //   let value = values.value;
+        //   if (typeof value !== 'number') return;
+        //   if (value > 100) value = 100;
+        //   if (value < 0) value = 0;
+        //   let optionId = values.row.id;
+        //   let factorName = values.field;
+        //   let factorId = values.row.factors.find((f => f.name === factorName)).id;
+        //   UpdateFactorScore(decisionId, optionId, factorId, value);
+        // }}
+        /> :
+        <Grid item container justifyContent='center' alignItems='center' ><Typography variant="body1" >No content</Typography></Grid>
+      }
+    </Grid>
   )
 }
 
--- src/components/Factors/Factors.jsx
+++ src/components/Factors/Factors.jsx
@@ -1,125 +1,125 @@
-import React,{useState,useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import { DecisionsContext } from '../../context/DecisionsContext';
-import { Grid,Button,TextField,IconButton,Typography,Slider,Divider} from '@mui/material'
+import { Grid, Button, TextField, IconButton, Typography, Slider, Divider } from '@mui/material'
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-const Factors = ({decisionId,factors}) => {
-  const {AddFactor,UpdateFactorName,RemoveFactor,AdjustFactorImportance} = useContext(DecisionsContext);
-  const [newFactorName,setNewFactorName] = useState('');
-  const [onUpdateOptionId,setOnUpdateOptionId] = useState(null);
-  const [editFactorName,setEditFactorName] = useState('');
+const Factors = ({ decisionId, factors }) => {
+  const { AddFactor, UpdateFactorName, RemoveFactor, AdjustFactorImportance } = useContext(DecisionsContext);
+  const [newFactorName, setNewFactorName] = useState('');
+  const [onUpdateOptionId, setOnUpdateOptionId] = useState(null);
+  const [editFactorName, setEditFactorName] = useState('');
 
-  const IsDuplicate = (name) =>{
-    const result = factors.find((f=>f.name===name));
-    if(result) return true;
+  const IsDuplicate = (name) => {
+    const result = factors.find((f => f.name === name));
+    if (result) return true;
     return false;
   }
-  const IsTheSameAsBefore = (factorId,name) =>{
-    const factor = factors.find((f=>f.id===factorId));
+  const IsTheSameAsBefore = (factorId, name) => {
+    const factor = factors.find((f => f.id === factorId));
     return factor?.name === name;
   }
 
   const AddNewFactorHandler = () => {
-    if(newFactorName==='') return;
-    if(IsDuplicate(newFactorName)){ alert('Name duplicate');return;} 
-    AddFactor(decisionId,newFactorName);
+    if (newFactorName === '') return;
+    if (IsDuplicate(newFactorName)) { alert('Name duplicate'); return; }
+    AddFactor(decisionId, newFactorName);
     setNewFactorName('');
   }
   const UpdateFactorHandler = () => {
-    if(IsDuplicate(editFactorName)){
-    if(IsTheSameAsBefore(editFactorName)){
+    if (IsDuplicate(editFactorName)) {
+      if (IsTheSameAsBefore(editFactorName)) {
+        return;
+      }
+      alert('Name duplicate');
       return;
     }
-    alert('Name duplicate'); 
-    return;
-  } 
-    UpdateFactorName(decisionId,onUpdateOptionId,editFactorName);
-   // setOnUpdateOptionId(null);
+    UpdateFactorName(decisionId, onUpdateOptionId, editFactorName);
+    // setOnUpdateOptionId(null);
   }
   const DeleteFactorHandler = (id) => {
-    RemoveFactor(decisionId,id)
+    RemoveFactor(decisionId, id)
   }
-//   const debounce=(func, timeout = 800)=>{
-//   let timer;
-//   return (...args) => {
-//     clearTimeout(timer);
-//     timer = setTimeout(() => { func.apply(this, args); }, timeout);
-//   };
-// }
-// const saveInput=(factorId,value)=>{
-//     AdjustFactorImportance(decisionId,factorId,value);
-// }
+  //   const debounce=(func, timeout = 800)=>{
+  //   let timer;
+  //   return (...args) => {
+  //     clearTimeout(timer);
+  //     timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  //   };
+  // }
+  // const saveInput=(factorId,value)=>{
+  //     AdjustFactorImportance(decisionId,factorId,value);
+  // }
 
-// const processChange = debounce((factorId,value) => saveInput(factorId,value));
+  // const processChange = debounce((factorId,value) => saveInput(factorId,value));
 
   // Option component
-  const Factor = (factor,_) => (
-    <Grid key={factor.id} item container sx={{ mt:1, height: '40px'}}>
-      <Grid item xs={7} md={8}  sx={{ p:1,  height: '40px' ,display: 'flex', justifyContent: 'center',alignItems:'center'}}>
-        <TextField 
-            required
-            defaultValue={factor.name}
-            variant='outlined'
-            size='small' 
-            fullWidth
-            onChange={(e)=>setEditFactorName(e.target.value)}
-            onFocus={(e)=>{setOnUpdateOptionId(factor.id);setEditFactorName(e.target.value)}}
-            onBlur={UpdateFactorHandler}
-          />
-        <IconButton onClick={()=>DeleteFactorHandler(factor.id)}><DeleteOutlineIcon color='primary'/></IconButton>
+  const Factor = (factor, _) => (
+    <Grid key={factor.id} item container sx={{ mt: 1, height: '40px' }}>
+      <Grid item xs={7} md={8} sx={{ p: 1, height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <TextField
+          required
+          defaultValue={factor.name}
+          variant='outlined'
+          size='small'
+          fullWidth
+          onChange={(e) => setEditFactorName(e.target.value)}
+          onFocus={(e) => { setOnUpdateOptionId(factor.id); setEditFactorName(e.target.value) }}
+          onBlur={UpdateFactorHandler}
+        />
+        <IconButton onClick={() => DeleteFactorHandler(factor.id)}><DeleteOutlineIcon color='primary' /></IconButton>
       </Grid>
-      <Grid item xs={5} md={4}  sx={{ height: '40px' ,display: 'flex',pl:'4px',justifyContent: 'center',alignItems:'center'}}>
-        <span style={{width:'30px'}}>{factor.weight}</span> 
-        <Slider 
+      <Grid item xs={5} md={4} sx={{ height: '40px', display: 'flex', pl: '4px', justifyContent: 'center', alignItems: 'center' }}>
+        <span style={{ width: '30px' }}>{factor.weight}</span>
+        <Slider
           key={`slider-${'sdd'}`}
-          size="small" 
-          sx={{ml:1, width:'70%'}} 
+          size="small"
+          sx={{ ml: 1, width: '70%' }}
           // TODO: Fix Error :MUI: A component is changing the default value state of an uncontrolled Slider after being initialized. To suppress this warning opt to use a controlled Slider.
-          defaultValue={factor.weight} 
+          defaultValue={factor.weight}
           valueLabelDisplay="auto"
           // onChange={(e)=>processChange(factor.id,e.target.value)}
-         onChangeCommitted={(_,value)=>AdjustFactorImportance(decisionId,factor.id,value)}
-         />
+          onChangeCommitted={(_, value) => AdjustFactorImportance(decisionId, factor.id, value)}
+        />
       </Grid>
     </Grid>);
 
   return (
-      <Grid item md={5.9} container sx={{  
-        // maxHeight: '700px',
-        padding: '0 .5rem 0 .5rem ',
-        overflowY: 'auto',
-        overflowX: 'hidden',
-        }} >
-        
-        {/* Header */}
-        <Grid item container  sx={{ height: '20px',m:'10px'}}>
-        <Grid item container justifyContent='center' alignItems='center' xs={7} md={8}  sx={{ pl:'4px'}}>
+    <Grid item md={5.9} container sx={{
+      // maxHeight: '700px',
+      padding: '0 .5rem 0 .5rem ',
+      overflowY: 'auto',
+      overflowX: 'hidden',
+    }} >
+
+      {/* Header */}
+      <Grid item container sx={{ height: '20px', m: '10px' }}>
+        <Grid item container justifyContent='center' alignItems='center' xs={7} md={8} sx={{ pl: '4px' }}>
           <Typography variant='body1' fontWeight="lg" ><strong>Factors:</strong></Typography>
         </Grid>
-        <Grid item container justifyContent='center' alignItems='center' xs={5} md={4}  sx={{ pl:'4px'}}>
-        <Typography variant='body1' fontWeight="lg" ><strong>Importance:</strong></Typography>
-          </Grid>
-          <Grid item xs={12} >
-              <Divider sx={{ml:'20px', mr:'20px'}}/>
-          </Grid>
+        <Grid item container justifyContent='center' alignItems='center' xs={5} md={4} sx={{ pl: '4px' }}>
+          <Typography variant='body1' fontWeight="lg" ><strong>Importance:</strong></Typography>
+        </Grid>
+        <Grid item xs={12} >
+          <Divider sx={{ ml: '20px', mr: '20px' }} />
+        </Grid>
       </Grid>
       {/* Factors */}
-      {factors!==[]&&factors.map((factor,_)=>{
-        return Factor(factor,_)
+      {factors !== [] && factors.map((factor, _) => {
+        return Factor(factor, _)
       })}
-          {/* Add */}
-          <Grid item container sx={{ mt:1, height: '40px'}}>
-      <Grid item xs={6.9} md={7.9}  sx={{ p:1,  height: '40px' ,display: 'flex', justifyContent: 'center',alignItems:'center'}}>
-      <TextField 
+      {/* Add */}
+      <Grid item container sx={{ mt: 1, height: '40px' }}>
+        <Grid item xs={6.9} md={7.9} sx={{ p: 1, height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <TextField
             label="New option"
             value={newFactorName}
             variant='outlined'
-            size='small' 
+            size='small'
             fullWidth
-            onChange={(e)=>setNewFactorName(e.target.value)}
+            onChange={(e) => setNewFactorName(e.target.value)}
           />
-      </Grid>
-      <Grid item xs={4.9} md={3.9}  sx={{ height: '40px' ,display: 'flex',pl:'4px',justifyContent: 'center',alignItems:'center'}}><Button onClick={AddNewFactorHandler} fullWidth variant='outlined'><AddCircleOutlineIcon /></Button></Grid>      
+        </Grid>
+        <Grid item xs={4.9} md={3.9} sx={{ height: '40px', display: 'flex', pl: '4px', justifyContent: 'center', alignItems: 'center' }}><Button onClick={AddNewFactorHandler} fullWidth variant='outlined'><AddCircleOutlineIcon /></Button></Grid>
       </Grid>
     </Grid>
   )
